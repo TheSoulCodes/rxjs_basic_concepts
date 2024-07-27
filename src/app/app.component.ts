401bede8f33e04c06ed7e27829038e5659dd44de
TheSoulCodes <souravmandal983@gmail.com>
2024-07-27 12:33:50 +0530

    Home page restructuring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  RouterLinkActive,
-  RouterOutlet,
-  Router,
-  RouterLink,
-} from '@angular/router';
-import { NgFor } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterOutlet, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FormsModule, NgFor],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'rxjs_basic_concepts';
-  selectedValue: string = 'Select';
 
-  operatorsList: any = [
-    { label: 'operator_basics', path: '/operator_basics' },
-    { label: 'from', path: '/from' },
-    { label: 'new_observable', path: '/new_observable' },
-    { label: 'functionObs', path: '/function_observable' },
-  ];
-
-  pipeableList: any = [];
-
-  miscellaneousList: any = [{ label: 'unsubscribe', path: '/unsubscribe' }];
-
-  constructor(private router: Router) {}
+  constructor() {}
   ngOnInit(): void {}
-
-  onSelectChange(event: Event): void {
-    const target = event.target as HTMLSelectElement;
-    const path = target.value;
-    this.router.navigate([path]);
-  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import {
+  RouterLinkActive,
+  RouterOutlet,
+  Router,
+  RouterLink,
+} from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  operatorsList: any = [
+    { id: 1, label: 'operator_basics', path: '/operator_basics' },
+    { id: 2, label: 'from', path: '/from' },
+    { id: 3, label: 'new_observable', path: '/new_observable' },
+    { id: 4, label: 'functionObs', path: '/function_observable' },
+  ];
 
+  pipeableList: any = [];
+
+  miscellaneousList: any = [
+    { id: 1, label: 'unsubscribe', path: '/unsubscribe' },
+  ];
 }
